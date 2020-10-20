33285b56ceb1fd2f82b28851e0e3dabfb986fcff
Tunc Celik <58709364+TuncCelik@users.noreply.github.com>
2020-10-20 10:29:37 +0100

    Update Hello2.java

--- src/test/java/Hello2.java
+++ src/test/java/Hello2.java
@@ -2,5 +2,7 @@ public class Hello2 {
     public static void main(String[] args) {
         System.out.println("Test2");
         System.out.println("testChanged");
+        //Changed and sent to ide
+        
     }
 }
